31d592a11a1c85f3ed6743b11cafaa9aaafdcf81
adii-1902 <kumbharaditya9903@gmail.com>
2025-01-14 19:50:34 +0530

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUse
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice.js';
 import { useSelector } from 'react-redux';
+import { HiChartPie } from 'react-icons/hi';
 
 export default function DashSidebar() {
     const dispatch = useDispatch();
@@ -40,6 +41,19 @@ export default function DashSidebar() {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to={'/dashboard?tab=dash'}>
+                                <Sidebar.Item
+                                    active={tab === 'dash' || !tab}
+                                    icon={HiChartPie}
+                                    as='div'
+                                >
+                                    Dashboard
+                                </Sidebar.Item>
+                            </Link>
+                        )
+                    }
                     <Link to='/dashboard?tab=profile'>
                         <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
                             Profile
