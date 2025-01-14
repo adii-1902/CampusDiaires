387c9b35cee32bbd6d1500ac622b701a2b18f78f
adii-1902 <kumbharaditya9903@gmail.com>
2025-01-14 18:17:35 +0530

    add comment section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice.js';
 import { useSelector } from 'react-redux';
@@ -56,15 +56,28 @@ export default function DashSidebar() {
                         </Link>)
                     }
                     {
-                        currentUser.isAdmin && (<Link to='/dashboard?tab=users'>
-                            <Sidebar.Item
-                                active={tab === 'users'}
-                                icon={HiOutlineUserGroup}
-                                as='div'>
-                                Users
-                            </Sidebar.Item>
-                        </Link>)
+                        currentUser.isAdmin && (
+                            <>
+                                <Link to='/dashboard?tab=users'>
+                                    <Sidebar.Item
+                                        active={tab === 'users'}
+                                        icon={HiOutlineUserGroup}
+                                        as='div'>
+                                        Users
+                                    </Sidebar.Item>
+                                </Link>
+                                <Link to='/dashboard?tab=comments'>
+                                    <Sidebar.Item
+                                        active={tab === 'comments'}
+                                        icon={HiAnnotation}
+                                        as='div'>
+                                        Comments
+                                    </Sidebar.Item>
+                                </Link>
+                            </>
+                        )
                     }
+
                     <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className='cursor-pointer'>
                         Sign Out
                     </Sidebar.Item>
