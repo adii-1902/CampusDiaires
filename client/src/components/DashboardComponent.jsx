9f67cecc35c6d63644059bd4ec7010ffdc07fd87
adii-1902 <kumbharaditya9903@gmail.com>
2025-01-20 09:28:28 +0530

    changes in dashboard component

--- client/src/components/DashboardComponent.jsx
+++ client/src/components/DashboardComponent.jsx
@@ -15,6 +15,7 @@ export default function DashboardComponent() {
     const [lastMonthComments, setLastMonthComments] = useState(0);
     const [lastMonthPosts, setLastMonthPosts] = useState(0);
     const { currentUser } = useSelector((state) => state.user);
+
     const [access, setAccess] = useState({});
 
     useEffect(() => {
